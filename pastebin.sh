697ac55adee2e9931daadb7f8edff8967bca71bd
Patrick Hudson <patrick@hudson.bz>
2015-04-20 05:17:28 -0500

    pastebin

---
+++ pastebin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cyan=$(tput setaf 6)
+normal=$(tput sgr0)
+printf "${cyan}Enter the path of the file you want to upload : ${normal}"
+read -r FILE
+curl -d title="$(date '+%m-%W-%Y-%X')" -d private=1 -d expire=45 --data-urlencode text@$FILE http://txtsnip.com/api/create
