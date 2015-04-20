339efca4697f871c7e85a2c35e7746c4a0e858f5
Patrick Hudson <patrick@hudson.bz>
2015-04-20 05:49:25 -0500

    password update

--- pastebin.sh
+++ pastebin.sh
@@ -3,4 +3,4 @@ cyan=$(tput setaf 6)
 normal=$(tput sgr0)
 printf "${cyan}Enter the path of the file you want to upload : ${normal}"
 read -r FILE
-curl -d title="$(date '+%m-%W-%Y-%X')" -d private=1 -d expire=45 --data-urlencode text@$FILE http://racker@rackspace:txtsnip.com/api/create
+curl -d title="$(date '+%m-%W-%Y-%X')" -d private=1 -d expire=45 --data-urlencode text@$FILE -u racker:rackspace http://txtsnip.com/api/create
