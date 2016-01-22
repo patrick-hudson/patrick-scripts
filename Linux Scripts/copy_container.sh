0b1ef1ca9718c1183cfc03317ff330fc08531aec
Patrick Hudson <patrick@hudson.bz>
2016-01-22 05:14:33 -0600

    fixed a naming bug

--- Linux Scripts/copy_container.sh
+++ Linux Scripts/copy_container.sh
@@ -1,20 +1,17 @@
 auth() {
     read -p "Whats your username: " username    
     read -p "Whats your APIkey: " APIkey
-    read -p "Which Datacenter do you want the Container in? " dc
 }
 
 copycontainer(){
     read -p "What container do you want to copy from? " ogcontainer
     read -p "What container do you want to copy to? " newcontainer
-    for file in $(swiftly --auth-user=$username --auth-key=$APIkey --auth-url="https://identity.api.rackspacecloud.com/v2.0" get test_tiles| sed 's/ /_-_/g'); do
+    for file in $(swiftly --auth-user=$username --auth-key=$APIkey --auth-url="https://identity.api.rackspacecloud.com/v2.0" get $ogcontainer| sed 's/ /_-_/g'); do
         file=$(echo $file |  sed 's/_-_/ /g')
-        file=$(echo $file |  sed 's/ /%20/g')
-        swiftly --auth-user=$username --auth-key=$APIkey --auth-url="https://identity.api.rackspacecloud.com/v2.0" put -e -H "X-Copy-From:/$ogcontainer/$file" $newcontainer/$file
+        swiftly --auth-user=$username --auth-key=$APIkey --auth-url="https://identity.api.rackspacecloud.com/v2.0" put -e -H "X-Copy-From:/$ogcontainer/$file" "$newcontainer/$file"
         echo "Copied $file from $ogcontainer to $newcontainer"
     done
 }
 
 auth
 copycontainer
-
